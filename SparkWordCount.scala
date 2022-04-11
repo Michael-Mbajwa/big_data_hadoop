3e2882fe85b3e0d5064065c44ac497f03e4cf2fa
Michael Mbajwa <michaelmbajwa@gmail.com>
2022-04-11 02:10:05 +0200

    A scala object that extracts words and numbers in order of their counts

---
+++ SparkWordCount.scala
@@ -0,0 +1,36 @@
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.SparkContext._
+import scala.util.matching.Regex
+
+
+object SparkWordCount {
+  def main(args: Array[String]) {
+    if (args.length < 2) {
+      System.err.println("Correct arguments: <input-directory> <output-directory>")
+      System.exit(1)
+    }
+
+    val sparkConf = new SparkConf().setAppName("SparkWordCount")
+    val ctx = new SparkContext(sparkConf)
+    val textFile = ctx.textFile(args(0))
+    
+    val alphaRegex = """[a-z-_]+""".r
+    val numRegex = """[-+]?[0-9]+[.]{0,1}[0-9]*""".r
+
+    val origFile = textFile.flatMap(line => line.split(" "))
+    val file = origFile.map(word => word.toLowerCase())
+    
+    val alphaFile = file.filter(x => (alphaRegex.pattern.matcher(x).matches))
+    val numFile = file.filter(x => (numRegex.pattern.matcher(x).matches))
+
+    val alphaCounts = alphaFile.map(word => (word, 1)).reduceByKey(_ + _).sortBy(_._2,false).take(1000)
+    val numericCounts = numFile.map(word => (word, 1)).reduceByKey(_ + _).sortBy(_._2,false).take(1000)
+
+    val alphaFinal = ctx.parallelize(alphaCounts, 2)
+    val numFinal = ctx.parallelize(numericCounts, 2)
+
+    alphaFinal.repartition(1).saveAsTextFile(args(1))
+    numFinal.repartition(1).saveAsTextFile(args(2))
+    ctx.stop()
+  }
+}
